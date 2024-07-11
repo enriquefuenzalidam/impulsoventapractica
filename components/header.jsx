5d26eecbdea808f2f52f59108d1c08d86a5922f2
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-07-11 12:11:38 -0400

    oto commit

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import netlifyLogo from 'public/images/logo-dba.ico';
-import githubLogo from 'public/images/github-mark-white.svg';
+import impulsoVentasLogo from 'public/images/logo-dba.ico';
 
 const navItems = [
     { linkText: 'Inicio', href: '#' },
@@ -14,17 +13,16 @@ export function Header() {
     return (
         <nav className="bg-[#0B0B5C]">
             <section className="max-w-screen-lg mx-auto p-10 text-white">
-            <p className="inline-block w-4/12 "><Image className=" inline-block h-12 w-auto" src={netlifyLogo} alt="Netlify logo" /><Link className="inline-block  font-semibold no-underline ml-1 text-xl uppercase" href="/">Impulso Ventas</Link></p>
-
-            {!!navItems?.length && (
+                <p className="inline-block w-4/12 "><Image className=" inline-block h-12 w-auto" src={impulsoVentasLogo} alt="Netlify logo" /><Link className="inline-block  font-semibold no-underline ml-1 text-xl uppercase" href="/">Impulso Ventas</Link></p>
+                {!!navItems?.length && (
                 <ul className="w-8/12 inline-block text-right text-lg">
                     {navItems.map((item, index) => (
-                        <li key={index} className=" inline-block  text-center">
-                            <Link href={item.href} className="inline-block px-1.5 py-1 transition hover:opacity-80 sm:py-2 no-underline" > {item.linkText} </Link>
+                        <li key={index} className=" inline-block text-right px-3">
+                            <Link href={item.href} className="inline-block py-1 sm:py-2 transition hover:opacity-80 no-underline text-nowrap" > {item.linkText} </Link>
                         </li>
                     ))}
                 </ul>
-            )}
+                )}
             </section>
         </nav>
     );
