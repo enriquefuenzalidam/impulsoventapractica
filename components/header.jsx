d4d32a3e8506ae5284f56b885ed1ad45b002d979
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-07-24 09:51:45 -0400

    dsd

--- components/header.jsx
+++ components/header.jsx
@@ -23,7 +23,6 @@ export function Header() {
                     <p className="inline-block w-2/12 sm:w-2/12 md:w-auto lg:w-auto "><Image className=" inline-block h-8 w-auto" src={impulsoVentasLogo} alt="Impulso Ventas Logo" /></p>
                     <p className="inline-block w-8/12 sm:w-8/12 md:w-auto lg:w-auto text-center sm:text-center md:text-left lg:text-left ml-2"><Link className="inline-block sm:inline-block md:inline-block lg:inline-block font-light tracking-widest no-underline ml-1 text-2xl text-white text-opacity-80 capitalize" href="/">I<em>mpulso</em> V<em>entas</em></Link></p>
                     <div className="w-2/12 sm:w-2/12 md:w-auto lg:w-auto inline-block sm:inline-block md:hidden lg:hidden text-right">
-                    {/* This the hamburger menu button that appears as the window gets narrow. As it is clicked, it should display the UL that hiddes as the window gets narrow, as a dropdown menu */}
                         <p className="inline-block cursor-pointer" onClick={() => setIsOpen(!isOpen)}><Image className=" inline-block h-8 w-auto" src={hamburgerMenu} alt="Menu" /></p>
                     </div>
                 </div>
@@ -41,7 +40,7 @@ export function Header() {
             </section>
 
             {!!navItems?.length && (
-            <ul className={`md:hidden lg:hidden bg-[#0B0B5C] overflow-hidden transition-all duration-300 ease-in-out ${
+            <ul className={`bg-[#0B0B5C] md:hidden lg:hidden overflow-hidden transition-all duration-300 ease-in-out ${
                 isOpen ? 'max-h-screen opacity-100 py-5 px-6' : 'max-h-0 opacity-0'
             }`}>
                 {navItems.map((item, index) => (
